115dd26dc5050937d887509eaf05d21b99facc77
Michał Witczak <michalwitczak19@gmail.com>
2022-06-11 17:58:44 +0200

    Students

--- Library/Controllers/StudentsController.cs
+++ Library/Controllers/StudentsController.cs
@@ -1,42 +1,38 @@
-﻿using Library.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using Library.Data;
 using Library.Models;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
 {
-    public class BooksController : Controller
+    public class StudentsController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public BooksController(ApplicationDbContext context)
+        public StudentsController(ApplicationDbContext context)
         {
             _context = context;
         }
-
         public IActionResult Index()
         {
-            return View(_context.Books.ToList());
+            return View(_context.Students.ToList());
         }
-
         public IActionResult Add()
         {
             return View();
         }
-
         [HttpPost]
-        public IActionResult Add(Book book)
+        public IActionResult Add(Student student)
         {
-            _context.Books.Add(book);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        
-        public IActionResult Delete(int bookId)
-        {
-            _context.Books.Remove(new Book { Id=bookId });
+            _context.Students.Add(student);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        public IActionResult Delete(int studentId)
+        {
+            _context.Students.Remove(new Student { Id = studentId });
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
